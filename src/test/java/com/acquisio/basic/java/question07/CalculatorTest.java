cc390766405cddb560e282ce7633e38f80af8430
Alfred Leung <alfred_tm_leung@hotmail.com>
2018-11-08 19:11:33 -0500

    Add solution for question 7

--- src/test/java/com/acquisio/basic/java/question07/CalculatorTest.java
+++ src/test/java/com/acquisio/basic/java/question07/CalculatorTest.java
@@ -1,6 +1,36 @@
 package com.acquisio.basic.java.question07;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class CalculatorTest {
 
-    // TODO: Insert your code here.
+    @Test
+    public void add() throws Exception {
+        assertEquals(new Integer(5), Calculator.applyOperation(1, 4, '+'));
+    }
+    
+    @Test
+    public void subtract() throws Exception {
+        assertEquals(new Integer(-3), Calculator.applyOperation(1, 4, '-'));
+    }
+    
+    @Test
+    public void multiply() throws Exception {
+        assertEquals(new Integer(4), Calculator.applyOperation(1, 4, '*'));
+    }
+    
+    @Test
+    public void divide() throws Exception {
+        assertEquals(new Integer(0), Calculator.applyOperation(1, 4, '/'));
+        assertEquals(new Integer(1), Calculator.applyOperation(4, 3, '/'));
+        assertEquals(new Integer(2), Calculator.applyOperation(4, 2, '/'));
+        assertNull(Calculator.applyOperation(4, 0, '/'));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void unknownOperation() throws Exception {
+        Calculator.applyOperation(1, 4, 'a');
+    }
 }
